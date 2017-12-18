d5d900640f931e8a39671619c4ecb459a4db209b
selvarajChinnasamy <selvaraj1234597@gmail.com>
2017-12-18 16:46:55 +0530

    Commit

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -11,21 +11,18 @@ export class FormsComponent implements OnInit {
 
   nameForm: FormGroup;
   status: boolean = false;
-  status1: boolean = false;
   name_input = '';
   names;
-  constructor(private fb: FormBuilder) {
-    // localStorage.setItem("names",JSON.stringify(this.name));
-    this.names = JSON.parse(localStorage.getItem("names"));
-    console.log(this.names);
-  }
+  error: string = '';
+  err: boolean = false;
+  constructor(private fb: FormBuilder) { }
 
   buildForm() {
     this.nameForm = this.fb.group({
       name: ['', Validators.compose([
         Validators.required,
         Validators.pattern("[A-Z]+[A-Za-z0-9-&$_]*"),
-      Validators.pattern(/\B([A-Za-z]{1,10})/),
+        Validators.pattern(/\B([A-Za-z]){1,10}/),
         this.ValidateName,
       ])],
     });
@@ -33,42 +30,67 @@ export class FormsComponent implements OnInit {
 
   ngOnInit() {
     this.buildForm();
+    this.err=false;
   }
- 
-  ValidateName(c: AbstractControl): { [key: string]: boolean} | null
-  {
-    if((c.value!==undefined) && (isNaN(c.value)))
-    {
-      var count=0;
+
+  ValidateName(c: AbstractControl): { [key: string]: boolean } | null {
+    if ((c.value !== undefined) && (isNaN(c.value))) {
+      var count = 0;
       var names = JSON.parse(localStorage.getItem("names"));
-      for(let i=0;i<names.length;i++)
-      {
-        if(c.value != names[i])
-        {
+      for (let i = 0; i < names.length; i++) {
+        if (c.value != names[i]) {
           count++;
-          console.log('names:'+names[i]);
+          console.log('names:' + names[i]);
         }
-        else{
+        else {
           break;
         }
       }
-    if(count<names.length)
-    {
-      return {error:true};
+      if (count < names.length) {
+        return { error: true };
+      }
+      return null;
     }
-     return null;
-    }
+  }
+
+
+  ErrorCheck() {
+    setTimeout(() => {
+      
+
+      console.log(this.nameForm.controls['name'].valid);
+      if (!this.nameForm.controls['name'].valid) {
+        this.err = true;
+        if (this.nameForm.get('name').hasError('required')) {
+          this.error = '*Name is Required';
+        }
+        else if (this.nameForm.get('name').hasError('pattern')) {
+          this.error = 'Patter Mismatch';
+        }
+        // else(this.nameForm.get('name').hasError('ValidateName'))
+        else {
+          this.error = 'Name already Presents';
+        }
+      }
+      else {
+        this.err = false;
+        this.error = '';
+      }
+
+
+    }, 3000);
   }
 
   FormSubmit(data) {
+    this.ErrorCheck();
     console.log(data);
     var name = JSON.parse(localStorage.getItem("names"));
-      name.push(data.name);
-      localStorage.setItem("names",JSON.stringify(name));
-      this.nameForm.reset();
-      this.status = true;
-      setTimeout(() => { this.status = false; }, 5000);
+    name.push(data.name);
+    localStorage.setItem("names", JSON.stringify(name));
+    this.nameForm.reset();
+    this.status = true;
+    setTimeout(() => { this.status = false; }, 5000);
   }
 
- 
+
 }
